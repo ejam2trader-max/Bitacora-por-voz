3a844cc318a0c460c6d1f584fe49c2ad23b540dc
ejam2trader-max <ejam2.trader@gmail.com>
2026-01-27 10:15:57 -0400

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const cacheName = 'trading-cache-v1';
-const staticAssets = ['./', './index.html', './manifest.json'];
+// Cambia 'v1' por 'v2' o cualquier número nuevo
+const CACHE_NAME = 'trading-v3'; 
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+// Este código ayuda a eliminar versiones viejas automáticamente
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    }));
+  }));
 });
 
 self.addEventListener('fetch', e => {
