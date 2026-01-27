b9d8326ec35ab3bbef185d4a7f39291001e43e0d
ejam2trader-max <ejam2.trader@gmail.com>
2026-01-27 11:14:05 -0400

    Update CACHE_NAME to 'trading-v6' and improve formatting

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-// Cambia 'v1' por 'v2' o cualquier número nuevo
-const CACHE_NAME = 'trading-v3'; 
+const CACHE_NAME = 'trading-v6';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-// Este código ayuda a eliminar versiones viejas automáticamente
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => {
-    return Promise.all(keys.map(key => {
-      if (key !== CACHE_NAME) return caches.delete(key);
-    }));
-  }));
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
